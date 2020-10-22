7dd586c6785090fee76cf5a264298304a20bdf59
Nisha271 <bukkenisha@gmail.com>
2020-10-22 22:35:42 +0530

    [Archana] Added: UC1 with First Name Validation

--- RegexPattern.java
+++ RegexPattern.java
@@ -1,8 +1,20 @@
-package com.regexpattern;
-
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class RegexPattern {
+	Scanner sc = new Scanner(System.in);
+
+	public String validateFirstName() {
+		System.out.println("Enter first Name: ");
+		String firstName = sc.next();
+		boolean b = Pattern.matches("^[A-Z]{1}[a-z]{2,}", firstName);
+		String result = (b) ? "valid" : "invalid" ;
+		return result;
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome Master");
+//		System.out.println("Welcome Master");
+		RegexPattern reg = new RegexPattern();
+		System.out.println("FirstName is : " + reg.validateFirstName());
 	}
 
 }
