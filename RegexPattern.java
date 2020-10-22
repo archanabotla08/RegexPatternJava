ca8705943f6be680948dad1d4eb45bcc7ae835b0
Nisha271 <bukkenisha@gmail.com>
2020-10-22 22:52:37 +0530

    [Archana] Added: UC3 with Email Valid

--- RegexPattern.java
+++ RegexPattern.java
@@ -18,11 +18,19 @@ public class RegexPattern {
 		String result = (b) ? "valid" : "invalid" ;
 		return result;
 	}
+	public String validateEmail() {
+		System.out.println("Enter Email Id Name: ");
+		String email = sc.next();
+		boolean b = Pattern.matches("^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\\@[a-zA-Z0-9]+([.]{1,2}[a-z]{2,}){0,2}", email);
+		String result = (b) ? "valid" : "invalid" ;
+		return result;
+	}
 	public static void main(String[] args) {
 //		System.out.println("Welcome Master");
 		RegexPattern reg = new RegexPattern();
 		System.out.println("FirstName is : " + reg.validateFirstName());
 		System.out.println("Lastname is : " + reg.validateLastName());
+		System.out.println("Email is : " + reg.validateEmail());
 	}
 
 }
